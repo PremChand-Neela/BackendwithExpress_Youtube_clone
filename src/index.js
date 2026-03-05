c63781cd27784368d9786def64eafb1dad6886f9
Neela Prem Chand <neelachandu149@gmail.com>
2026-03-05 14:18:39 +0530

    feat: set up Express server with middleware and error handling utilities
    - Updated package.json to include new dependencies: cookie-parser, cors, dotenv, express, and mongoose.
    - Created app.js to initialize Express app with CORS, cookie parsing, and body parsing middleware.
    - Added constants.js for database name configuration.
    - Implemented index.js to configure environment variables and establish MongoDB connection.
    - Introduced ApiError and ApiResponse utility classes for standardized error and response handling.
    - Added asyncHandler utility for handling asynchronous route handlers.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 7000 ,()=>{
+        console.log(`Server is running on port number ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err);
+    
+})
